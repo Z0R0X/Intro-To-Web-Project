2e41298aedb7bb2d9101b12ac9bbb0f3d11c9c67
Siraj Alaeli <ironsiraj222@gmail.com>
2024-11-20 12:01:31 +0200

    completed functionality of products page

--- Products/script.js
+++ Products/script.js
@@ -1,3 +1,27 @@
+let cart = [];
+
+function addToCart(id) {
+    let existingItem = cart.find(item => item === id);
+
+    if (existingItem) {
+        alert("Item is already in the cart!");
+    } else {
+        cart.push(id);
+        alert("Item added to the cart!");
+        saveCart();
+    }
+}
+
+function saveCart() {
+    localStorage.setItem('cart', JSON.stringify(cart));
+}
+
+function loadCart() {
+    let savedCart = localStorage.getItem('cart');
+    if (savedCart) {
+        cart = JSON.parse(savedCart);
+    }
+}
 function loadProducts()
 {
     fetch('products.json')
@@ -7,32 +31,35 @@ function loadProducts()
     let products = data.products
     categories.forEach(category => 
     {
-        $('#links').append(
-            '<section class="link">'+
-                '<img src="'+category.image+'" />'+
-                '<p>'+category.name+'</p>'+
-            '</section>');
+        var link = $("<section>").addClass("link");
+        var img = $("<img>").attr("src", category.image);
+        var a = $("<a>").attr("href", '#category'+(categories.indexOf(category)));
+        a.append(img);
+        var p = $("<p>").html(category.name);
+        link.append(a, p);
+        $('#links').append(link);
         $('main').append(
         '<div class="storePart">' +
             '<hr />'+
             '<h1>'+category.name+'</h1>'+
             '<section class="itemParent" id="category'+(categories.indexOf(category))+'">'+
             '</section>'+
-        '</div>');
-        console.log(categories.length-1);            
+        '</div>');     
     })
     products.forEach(product => {
         let catIndex = categories.findIndex(category => category.name === product.category);
-        $('#category' + catIndex).append(
-            '<div class="item">'+
-                '<img src="'+product.image+'" />'+
-                '<div>'+
-                    '<h4>'+product.name+'</h4>'+
-                    '<p>Price:'+product.price+'$</p>'+
-                    '<button>Add To Cart</button>'+
-                '</div>'+
-            '</div>'
-        );     
+        var div = $("<div>").addClass("item");
+        var img = $("<img>").attr("src", product.image);
+        var dChild = $("<div>");
+        var name = $("<h4>").html(product.name);
+        var price = $("<p>").html('<p>Price:'+product.price+'$</p>');
+        var btn = $("<button>").html("Add To Cart");
+        btn.click(()=>{ 
+            addToCart(product.id);
+        });
+        dChild.append(name, price, btn);
+        div.append(img, dChild);
+        $('#category' + catIndex).append(div); 
     });
   })
   .catch(error => console.error('Error loading JSON:', error));
