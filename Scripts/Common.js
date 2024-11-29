10f95f3c718c2ff8610c219a13341e4770ff0603
MahmoudSidawi <mahsid1234@gmail.com>
2024-11-29 18:35:40 +0200

    h

--- Scripts/Common.js
+++ Scripts/Common.js
@@ -3,15 +3,46 @@ document.addEventListener('DOMContentLoaded', function () {
     let loggedInUser = JSON.parse(localStorage.getItem('loggedInUser'));
 
     if (loggedInUser) {
-        loginButton.textContent = loggedInUser.name; 
+        loginButton.textContent = loggedInUser.name;
         loginButton.href = '#';
 
         loginButton.addEventListener('click', function () {
-            let Confirm = confirm('Do you want to log out?');
-            if (Confirm) {
-                localStorage.removeItem('loggedInUser'); 
-                window.location.reload();
-            }
+            LogoutConfirmation();
         });
     }
 });
+
+function LogoutConfirmation() {
+    let lay = document.createElement('div');
+    lay.className = 'lay';
+
+    let message = document.createElement('div');
+    message.className = 'logout';
+
+    let modalMessage = document.createElement('p');
+    modalMessage.textContent = 'do you want to log out?';
+
+    let confirmButton = document.createElement('button');
+    confirmButton.textContent = 'yes';
+    confirmButton.className = 'confirm-button';
+
+    let cancelButton = document.createElement('button');
+    cancelButton.textContent = 'cancel';
+    cancelButton.className = 'cancel-button';
+
+    message.appendChild(modalMessage);
+    message.appendChild(confirmButton);
+    message.appendChild(cancelButton);
+    lay.appendChild(message);
+    document.body.appendChild(lay);
+
+    confirmButton.addEventListener('click', function () {
+        localStorage.removeItem('loggedInUser'); 
+        document.body.removeChild(lay);
+        window.location.reload();
+    });
+
+    cancelButton.addEventListener('click', function () {
+        document.body.removeChild(lay);
+    });
+}
